16c45ff97940e9366864a3948788a15ca1b0e6fe
Hu Yuchen <huyc1998@gmail.com>
2020-07-14 23:14:20 +0800

    implement upload function

--- utils/gdrivefile_util.py
+++ utils/gdrivefile_util.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -6,7 +7,10 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
+from googleapiclient.http import MediaFileUpload
+
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
 
 def download_file():
     file_id = '0BwwA4oUTeiV1UVNwOHItT0xfa2M'
@@ -19,6 +23,43 @@ def download_file():
         print
         "Download %d%%." % int(status.progress() * 100)
 
+
+def upload_file(file_name, drive_service, folder_name, file_path, file_mime_type):
+    # Search for folder id in Drive
+    page_token = None
+    folders = []
+    while True:
+        response = drive_service.files().list(
+            q=f"mimeType='application/vnd.google-apps.folder' and name='{folder_name}'",
+            spaces='drive',
+            fields='nextPageToken, files(id, name)',
+            pageToken=page_token).execute()
+        for folder in response.get('files', []):
+            folders.append(folder)
+        page_token = response.get('nextPageToken', None)
+        if page_token is None:
+            break
+    if not len(folders):
+        raise Exception('Specified Google Drive folder not found')
+    if len(folders) > 1:
+        raise Exception('Exists duplicate folders with the given name')
+    folder_id = ''
+    for folder in folders:
+        folder_id = folder.get('id')
+    # Upload file to designated folder
+    file_metadata = {
+        'name': [file_name],
+        'parents': [folder_id]
+    }
+    media = MediaFileUpload(file_path,
+                            mimetype=file_mime_type,
+                            resumable=True)
+    file = drive_service.files().create(body=file_metadata,
+                                        media_body=media,
+                                        fields='id').execute()
+    print(file)
+
+
 def main():
     """Shows basic usage of the Drive v3 API.
     Prints the names and ids of the first 10 files the user has access to.
@@ -27,6 +68,7 @@ def main():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
+    SCOPES = 'https://www.googleapis.com/auth/drive.file'
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
@@ -48,6 +90,9 @@ def main():
     results = service.files().list(
         pageSize=10, fields="nextPageToken, files(id, name)").execute()
     items = results.get('files', [])
+    upload_file('episode1.mp4', service, 'test',
+                '/Users/brebeek/infinite_challenge/temp/episodes/processed/episode1.mp4',
+                'video/mp4')
 
     if not items:
         print('No files found.')
